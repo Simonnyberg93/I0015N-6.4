904a518be192c1c50d736083c457988c43aca03e
simnyb-9 <63825366+simnyb-9@users.noreply.github.com>
2021-05-14 14:33:47 +0200

    asdasd

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -2,27 +2,27 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+
 
 public class GridTest {
 
     public static URL url;
     public RemoteWebDriver driver;
 
+
     @BeforeAll
     public static void setUp(){
         try {
-            url = new URL("http://3.238.176.22:4444/wd/hub");
+            url = new URL("http://44.192.54.57:4444/wd/hub");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -38,6 +38,7 @@ public class GridTest {
         };
     }
 
+
     @DisplayName("Google search.")
     @ParameterizedTest(name = "Testing script 1 with {0}")
     @CsvSource({"firefox","chrome","opera","edge"})
@@ -45,19 +46,21 @@ public class GridTest {
         DesiredCapabilities cap = setupCapabilitys(browser);
         cap.setPlatform(Platform.LINUX);
         driver = new RemoteWebDriver(url, cap);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().setSize(new Dimension(1900, 1000));
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         driver.get("https://www.google.com");
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-       // driver.findElement(By.id("L2AGLb")).click();
+       // driver.findElement(By.xpath("//div//button[@id='L2AGLb']")).click();
         driver.findElement(By.name("q")).sendKeys("Software Testing");
         driver.findElement(By.name("q")).submit();
         driver.quit();
     }
+
 
     @DisplayName("Actitime create user.")
     @ParameterizedTest(name = "Testing script 2 with {0}")
@@ -66,8 +69,9 @@ public class GridTest {
         DesiredCapabilities cap = setupCapabilitys(browser);
         cap.setPlatform(Platform.LINUX);
         driver = new RemoteWebDriver(url, cap);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().setSize(new Dimension(1900, 1000));
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         driver.get("https://www.actitime.com/");
 
         driver.findElement(By.linkText("Try Free")).click();
@@ -81,8 +85,10 @@ public class GridTest {
         driver.findElement(By.id("last-name")).sendKeys("Doe");
         driver.findElement(By.id("email")).sendKeys("JohnDoe@hotmail.com");
         driver.findElement(By.id("company")).sendKeys("Test av IT AB");
+
         driver.quit();
     }
+
 
     @DisplayName("Order book on Adlibris.")
     @ParameterizedTest(name = "Testing script 3 with {0}")
@@ -91,18 +97,19 @@ public class GridTest {
         DesiredCapabilities cap = setupCapabilitys(browser);
         cap.setPlatform(Platform.LINUX);
         driver = new RemoteWebDriver(url, cap);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().setSize(new Dimension(1900, 1000));
-
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
         driver.get("https://Adlibris.Com/se/");
         driver.findElement(By.id("q")).sendKeys("praktisk mjukvarutestning");
         driver.findElement(By.id("q")).submit();
         driver.findElement(By.linkText("Praktisk mjukvarutestning")).click();
+
         driver.findElement(By.cssSelector(".product__add-to-cart:nth-child(1)")).click();
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Till kassan")));
+
         driver.findElement(By.cssSelector(".notifications-bar__added-to-cart__to-checkout")).click();
+        driver.quit();
 
     }
+
 }
